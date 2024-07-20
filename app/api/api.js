da090d3870737c9c3948b7297e2baf53419523c8
Lucas de Angelo <lucasdeangelo343@gmail.com>
2024-07-20 12:28:28 -0300

    Add - Cadastro de Usuário

---
+++ app/api/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3333';
+
+export const registerUser = (createUser) => {
+  return axios.post(`${API_URL}/user`, createUser);
+};
--- app/screens/cadastro.js
+++ app/screens/cadastro.js
@@ -1,26 +1,60 @@
-import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import React from 'react';
+import { StyleSheet, Image, Text, View, TextInput, Picker, TouchableOpacity, Alert } from 'react-native';
+import React, { useState } from 'react';
 import { useFonts, Nunito_500Medium } from '@expo-google-fonts/nunito';
 import { Poppins_700Bold } from '@expo-google-fonts/poppins';
 import { Link } from 'expo-router';
+import { useNavigation } from '@react-navigation/native';
+import { registerUser } from '../api/api';
 
 export default function Cadastro() {
-    const [fontLoaded] = useFonts({
-      Nunito_500Medium,
-      Poppins_700Bold
-    })
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [typeUser, settypeUser] = useState('');
+  const navigation = useNavigation();
+
+  const [fontLoaded] = useFonts({
+    Nunito_500Medium,
+    Poppins_700Bold
+  })
 
   if (!fontLoaded) {
     return null;
   }
+
   
+  const handleRegister = () => {
+    if (password !== confirmPassword) {
+      Alert.alert("Erro", "As senhas não coincidem");
+      return;
+    }
+
+    const createUser = {
+      name,
+      email,
+      password,
+      typeUser,
+    };
+
+    registerUser(createUser)
+      .then(response => {
+        Alert.alert('Sucesso', 'Cadastro realizado com sucesso!');
+        navigation.navigate('/screens/login');
+      })
+      .catch(error => {
+        Alert.alert('Erro', 'Erro ao realizar cadastro. Tente novamente.');
+        console.error(error);
+      });
+  };
+
   return (
     <View>
       <View style={styles.main}>
         <Image source={require('../../assets/images/icon.png')} style={styles.image}/>
         <View style={styles.mainText}>
           <Text style={styles.h1}>Vamos criar sua conta!</Text>           
-          <Text style={{...styles.txt, paddingTop: 5}}>Está pronto para navegar na adoração? insira as informações abaixo para criar sua conta!</Text>        
+          <Text style={{...styles.txt, paddingTop: 5}}>Está pronto adorar? insira as informações abaixo para criar sua conta!</Text>        
         </View>
 
         <View style={styles.form}>
@@ -29,37 +63,68 @@ export default function Cadastro() {
             <TextInput
               style={styles.input}
               placeholder={'Insira seu Nome...'}
+              value={name}
+              onChangeText={setName}
             />
           </View>
 
-          <View style={{paddingTop: 15}}>
+          <View style={{paddingTop: 12}}>
             <Text style={styles.h3}>Email</Text>
             <TextInput
               style={styles.input}
               placeholder={'Insira seu Email...'}
+              value={email}
+              onChangeText={setEmail}
+              keyboardType='email-address'
             />
           </View>
 
-          <View style={{paddingTop: 15}}>
+          <View style={{paddingTop: 12}}>
             <Text style={styles.h3}>Senha</Text>
             <TextInput
               style={styles.input}
               placeholder={'Insira sua Senha...'}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
             />
           </View>
 
-          <View style={{paddingTop: 15}}>
+          <View style={{paddingTop: 12}}>
             <Text style={styles.h3}>Confirme sua Senha</Text>
             <TextInput              
               style={styles.input}
               placeholder={'Insira sua Senha Novamente...'}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              secureTextEntry
             />
           </View>
 
-          <TouchableOpacity style={styles.btn} activeOpacity={0.7}>Cadastrar</TouchableOpacity>
+          <View style={{paddingTop: 12}}>
+            <Text style={styles.h3}>Tipo de usuário</Text>
+            <Picker     
+              selectedValue={typeUser}         
+              style={styles.input}
+              onValueChange={(itemValue) => settypeUser(itemValue)}
+            >
+              <Picker.Item label="Selecione o tipo de usuário" value=""/>
+              <Picker.Item label="Adorador" value="Adorador" />
+              <Picker.Item label="Regente" value="Regente" />
+              <Picker.Item label="Cantor" value="Cantor" />
+              <Picker.Item label="Músico" value="Músico" />
+              <Picker.Item label="Componente" value="Componente" />
+            </Picker>            
+          </View>
+
+          <TouchableOpacity style={styles.btn} activeOpacity={0.7} onPress={handleRegister}>Cadastrar</TouchableOpacity>
 
           <View style={{textAlign: 'center', paddingTop: 20}}>
             <Text style={styles.h3}>Ao se cadastrar você estará concordando com os termos de politica e privacidade!</Text>
+          </View>
+
+          <View style={{textAlign: 'center', paddingTop: 10}}>
+            <Text style={styles.h3}>Já tem conta? <Link style={styles.span} href={'/screens/login'}>Faça Login</Link></Text>
           </View>
         </View>
       </View>
@@ -69,7 +134,6 @@ export default function Cadastro() {
 
 const styles = StyleSheet.create({
   main: {
-    paddingTop: 40,
     display: 'flex',
     justifyContent: 'center',
     paddingHorizontal: 20
@@ -84,7 +148,8 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: 36,
     fontFamily: 'Poppins_700Bold',
-    lineHeight: 40    
+    lineHeight: 40,
+    textAlign: 'center'    
   },
   h3: {
     fontSize: 14,
@@ -95,7 +160,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontFamily: 'Nunito_500Medium',
     color: '#BFBFBF',
-    lineHeight: 14
+    lineHeight: 14,
+    textAlign: 'center'
   },
   form: {
     paddingTop: 20
@@ -120,5 +186,9 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontFamily: 'Nunito_500Medium',
     borderRadius: 12    
+  }, 
+  span: {
+    color: '#FFCB69',
+    fontFamily: 'Poppins_700Bold'    
   }
 })
